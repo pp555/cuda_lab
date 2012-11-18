93809e6f449a3214502a8df223f38b1f91d0b798
Piotr Pietruszka <piotrek555@gmail.com>
2012-11-18 14:10:51 +0100

    mierzenie czasow, CPU i GPU w jednym pliku

--- lab2/bmpfilter.cu
+++ lab2/bmpfilter.cu
@@ -9,23 +9,16 @@
 #include <string>
 
 #define INPUT_BMP_FILE "lenabig.bmp"
-#define OUTPUT_BMP_FILE "result.bmp"
+#define OUTPUT_BMP_FILE_CPU "result-cpu.bmp"
+#define OUTPUT_BMP_FILE_GPU "result-cuda.bmp"
 #define FILTER_WINDOW_SIZE 9
 
 #define BLOCK_SIZE 16
 
 using namespace std;
 
-
-
-
-void displayLastError(const string &msg)
-{
-	cout << "Last Error (" << msg << "):\t" << cudaGetErrorString(cudaGetLastError()) << endl;
-}
-
-#pragma pack(push)
-#pragma pack(1)
+//#pragma pack(push)
+//#pragma pack(1)
 struct FileHeader
 {
     char id[2];
@@ -33,59 +26,207 @@ struct FileHeader
     int sth;
     unsigned int offset;
 
-};// __attribute__ ((packed));
+}; __attribute__ ((packed));
 
 struct DibHeader
 {
     unsigned int dib_size;
     unsigned int width;
     unsigned int height;
-};// __attribute__ ((packed));
+}; __attribute__ ((packed));
 
 struct Pixel
 {
     unsigned char b;
     unsigned char g;
     unsigned char r;
-};// __attribute__ ((packed));
-#pragma pack(pop)
+}; __attribute__ ((packed));
+//#pragma pack(pop)
 
-
-
-__global__ void medianFilterCuda(Pixel *input, Pixel *output, int width, int height);
-
-
-void medianFilterGpu(Pixel *input, Pixel *output, int width, int height)
+struct Times // execution times [ms]
 {
+	float cuda;
+	float cudaOnlyComputation;
+	double cpu;
+} executionTimes;
 
-	size_t size = width * height * sizeof(Pixel);
-	Pixel *deviceInputImage;
-	cudaMalloc((void**)&deviceInputImage, size);
-	displayLastError("input image memory allocation");
-	cudaMemcpy(deviceInputImage, input, size, cudaMemcpyHostToDevice);
-	displayLastError("input image memcpy");
+void medianFilterCpu(Pixel *input, Pixel *output, int width, int height);
+Pixel medianFilterCpuHelper(Pixel *image, int width, int height, int y, int x);
+unsigned char selection(unsigned char window[FILTER_WINDOW_SIZE]);
+void medianFilterGpu(Pixel *input, Pixel *output, int width, int height);
+__global__ void medianFilterCuda(Pixel *input, Pixel *output, int width, int height);
+__device__ unsigned char selectionCuda(unsigned char window[FILTER_WINDOW_SIZE]);
+void displayLastError(const string &msg);
 
-	Pixel *deviceOutputImage;
-	cudaMalloc((void**)&deviceOutputImage, size);
-	displayLastError("output image memory allocation");
-
-	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-	dim3 dimGrid(width/BLOCK_SIZE, height/BLOCK_SIZE);
-	medianFilterCuda<<<dimGrid, dimBlock>>>(deviceInputImage, deviceOutputImage, width, height);
-	displayLastError("kernel");
-
-	cudaMemcpy(output, deviceOutputImage, size, cudaMemcpyDeviceToHost);
-	displayLastError("output image memcpy");
-
-
-	cudaFree(deviceInputImage);
-	displayLastError("freeing input image memory");
-	cudaFree(deviceOutputImage);
-	displayLastError("freeing output image memory");
-
+//get time (for CPU)
+double get_timestamp()
+{
+	struct timeval now;
+	gettimeofday(&now, NULL);
+	unsigned long long timeMicroseconds = now.tv_usec + now.tv_sec*1000000;
+	return (double)timeMicroseconds/1000.0;
 }
 
-__device__ unsigned char selectionCuda(unsigned char window[FILTER_WINDOW_SIZE])
+int main(int argc, char *argv[])
+{
+	//if(argc < 2)
+	//{
+	//	cerr << "no input file specified" <<  endl;
+	//	return 1;
+	//}
+	//opening input file
+    ifstream bmpFile(/*argv[1]*/INPUT_BMP_FILE, ios::in | ios::binary);
+    if(!bmpFile.is_open())
+    {
+        cerr << "file not opened" << endl;
+        exit(0);
+    }
+
+    //reading file headers
+    FileHeader header;
+    bmpFile.read((char*)&header, sizeof(header));
+
+    cout << "size:\t" << header.size << endl;
+    cout << "offset:\t" << header.offset << endl;
+
+    DibHeader dib;
+    bmpFile.read((char*)&dib, sizeof(dib));
+    cout << "DIB size:\t" << dib.dib_size << endl;
+    cout << "width:\t" << dib.width << endl;
+    cout << "height:\t" << dib.height << endl;
+
+    bmpFile.seekg(header.offset, ios_base::beg);
+
+    //reading image
+    Pixel *image = new Pixel[dib.height*dib.width];
+    for(int y = dib.height - 1; y >= 0 ; y--)
+    {
+        for(int x = 0; x < dib.width; x++)
+            bmpFile.read((char*)&(image[y*dib.width + x]), sizeof(Pixel));
+        bmpFile.seekg(dib.width%4, ios_base::cur);
+    }
+
+    int devCnt;
+    cudaGetDeviceCount(&devCnt);
+    if(devCnt == 0)
+    {
+		perror("No CUDA devices available -- exiting.");
+		return 1;
+    }
+	
+    Pixel *outputCpu = new Pixel[dib.width*dib.height];
+    Pixel *outputGpu = new Pixel[dib.width*dib.height];
+
+    medianFilterGpu(image, outputGpu, dib.width, dib.height);
+    medianFilterCpu(image, outputCpu, dib.width, dib.height);
+
+	cout << "times:\n";
+	cout << "CPU:\t\t" << executionTimes.cpu << endl;
+	cout << "GPU:\t\t" << executionTimes.cuda << endl;
+	cout << "GPU (computation):\t\t" << executionTimes.cudaOnlyComputation << endl;
+
+
+
+
+    //saving result bmp
+    ofstream bmpResultCpu(OUTPUT_BMP_FILE_CPU, ios::out | ios::binary);
+    ofstream bmpResultGpu(OUTPUT_BMP_FILE_GPU, ios::out | ios::binary);
+    char *buf = new char[header.offset];
+    char zerosBuf[4] = {0};
+    bmpFile.seekg(0, ios_base::beg);
+    bmpFile.read(buf, header.offset);
+    bmpResultCpu.write(buf, header.offset);
+    bmpResultGpu.write(buf, header.offset);
+	delete [] buf;
+
+    for(int y = dib.height - 1; y >= 0 ; y--)
+    {
+        for(int x = 0; x < dib.width; x++)
+        {
+            bmpResultCpu.write((char*)&(outputCpu[y*dib.width + x]), sizeof(Pixel));
+            bmpResultGpu.write((char*)&(outputGpu[y*dib.width + x]), sizeof(Pixel));
+        }
+        bmpResultCpu.write(zerosBuf, dib.width%4);
+        bmpResultGpu.write(zerosBuf, dib.width%4);
+    }
+	
+    bmpResultCpu.close();
+    bmpResultGpu.close();
+    bmpFile.close();
+
+    cout << endl;
+
+	delete [] image;
+	delete [] outputCpu;
+	delete [] outputGpu;
+
+    return 0;
+}
+
+void medianFilterCpu(Pixel *input, Pixel *output, int width, int height)
+{
+	double start = get_timestamp();
+
+    for(int y = height - 1; y >= 0 ; y--)
+    {
+        for(int x = 0; x < width; x++)
+        {
+			output[y*width+x] = medianFilterCpuHelper(input, width, height, y, x);
+        }
+    }
+	
+	executionTimes.cpu = get_timestamp() - start;
+}
+
+Pixel medianFilterCpuHelper(Pixel *image, int width, int height, int y, int x)
+{
+	if(x < 1 || y < 1 || x >= width || y >= height)
+		return image[y*width+x];
+
+	Pixel p;
+	unsigned char window[FILTER_WINDOW_SIZE];
+
+	//red
+	
+	window[0] = image[(y-1)%height*width+(x-1)%width].r;
+	window[1] = image[(y-1)%height*width+(x)%width].r;
+	window[2] = image[(y-1)%height*width+(x+1)%width].r;
+	window[3] = image[(y)%height*width+(x-1)%width].r;
+	window[4] = image[(y)%height*width+(x)%width].r;
+	window[5] = image[(y)%height*width+(x+1)%width].r;
+	window[6] = image[(y+1)%height*width+(x-1)%width].r;
+	window[7] = image[(y+1)%height*width+(x)%width].r;
+	window[8] = image[(y+1)%height*width+(x+1)%width].r;
+	p.r = selection(window);
+	
+	//green
+	window[0] = image[(y-1)%height*width+(x-1)%width].g;
+	window[1] = image[(y-1)%height*width+(x)%width].g;
+	window[2] = image[(y-1)%height*width+(x+1)%width].g;
+	window[3] = image[(y)%height*width+(x-1)%width].g;
+	window[4] = image[(y)%height*width+(x)%width].g;
+	window[5] = image[(y)%height*width+(x+1)%width].g;
+	window[6] = image[(y+1)%height*width+(x-1)%width].g;
+	window[7] = image[(y+1)%height*width+(x)%width].g;
+	window[8] = image[(y+1)%height*width+(x+1)%width].g;
+	p.g = selection(window);
+	
+	//blue
+	window[0] = image[(y-1)%height*width+(x-1)%width].b;
+	window[1] = image[(y-1)%height*width+(x)%width].b;
+	window[2] = image[(y-1)%height*width+(x+1)%width].b;
+	window[3] = image[(y)%height*width+(x-1)%width].b;
+	window[4] = image[(y)%height*width+(x)%width].b;
+	window[5] = image[(y)%height*width+(x+1)%width].b;
+	window[6] = image[(y+1)%height*width+(x-1)%width].b;
+	window[7] = image[(y+1)%height*width+(x)%width].b;
+	window[8] = image[(y+1)%height*width+(x+1)%width].b;
+	p.b = selection(window);
+	
+	return p;
+}
+
+unsigned char selection(unsigned char window[FILTER_WINDOW_SIZE])
 {
 	//http://en.wikipedia.org/wiki/Selection_algorithm
 	unsigned char minIndex, minValue;
@@ -105,6 +246,54 @@ __device__ unsigned char selectionCuda(unsigned char window[FILTER_WINDOW_SIZE])
 		window[i] = minValue;
 	}
 	return window[FILTER_WINDOW_SIZE / 2];
+}
+
+void medianFilterGpu(Pixel *input, Pixel *output, int width, int height)
+{
+	cudaEvent_t start, stop, startComp, stopComp;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventCreate(&startComp);
+	cudaEventCreate(&stopComp);
+	cudaEventRecord(start, 0);
+
+	size_t size = width * height * sizeof(Pixel);
+	Pixel *deviceInputImage;
+	cudaMalloc((void**)&deviceInputImage, size);
+	displayLastError("input image memory allocation");
+	cudaMemcpy(deviceInputImage, input, size, cudaMemcpyHostToDevice);
+	displayLastError("input image memcpy");
+
+	Pixel *deviceOutputImage;
+	cudaMalloc((void**)&deviceOutputImage, size);
+	displayLastError("output image memory allocation");
+
+	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+	dim3 dimGrid(width/BLOCK_SIZE, height/BLOCK_SIZE);
+	
+	cudaEventRecord(startComp, 0);
+	medianFilterCuda<<<dimGrid, dimBlock>>>(deviceInputImage, deviceOutputImage, width, height);
+	cudaEventRecord(stopComp, 0);
+	cudaEventSynchronize(stopComp);
+	cudaThreadSynchronize();
+	displayLastError("kernel");
+
+	cudaMemcpy(output, deviceOutputImage, size, cudaMemcpyDeviceToHost);
+	displayLastError("output image memcpy");
+
+	cudaFree(deviceInputImage);
+	displayLastError("freeing input image memory");
+	cudaFree(deviceOutputImage);
+	displayLastError("freeing output image memory");
+	
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	
+	cudaEventElapsedTime(&executionTimes.cuda, start, stop);
+	cudaEventElapsedTime(&executionTimes.cudaOnlyComputation, startComp, stopComp);
+
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
 }
 
 //kernel
@@ -155,96 +344,29 @@ __global__ void medianFilterCuda(Pixel *input, Pixel *output, int width, int hei
 	output[row * width + col] = result;
 }
 
-int main(int argc, char *argv[])
+__device__ unsigned char selectionCuda(unsigned char window[FILTER_WINDOW_SIZE])
 {
-	//if(argc < 2)
-	//{
-	//	cerr << "no input file specified" <<  endl;
-	//	return 1;
-	//}
-	//opening input file
-    ifstream bmpFile(/*argv[1]*/INPUT_BMP_FILE, ios::in | ios::binary);
-    if(!bmpFile.is_open())
-    {
-        cerr << "file not opened" << endl;
-        exit(0);
-    }
-
-    //reading file headers
-    FileHeader header;
-    bmpFile.read((char*)&header, sizeof(header));
-
-    cout << "size:\t" << header.size << endl;
-    cout << "offset:\t" << header.offset << endl;
-
-    DibHeader dib;
-    bmpFile.read((char*)&dib, sizeof(dib));
-    cout << "DIB size:\t" << dib.dib_size << endl;
-    cout << "width:\t" << dib.width << endl;
-    cout << "height:\t" << dib.height << endl;
-
-    bmpFile.seekg(header.offset, ios_base::beg);
-
-    //reading image
-    Pixel *image = new Pixel[dib.height*dib.width];
-    for(int y = dib.height - 1; y >= 0 ; y--)
-    {
-        for(int x = 0; x < dib.width; x++)
-            bmpFile.read((char*)&(image[y*dib.width + x]), sizeof(Pixel));
-        bmpFile.seekg(dib.width%4, ios_base::cur);
-    }
-
-    int devCnt;
-    cudaGetDeviceCount(&devCnt);
-    if(devCnt == 0)
-    {
-		perror("No CUDA devices available -- exiting.");
-		return 1;
-    }
-
-	
-	
-	
-
-    Pixel *output = new Pixel[dib.width*dib.height];
-    for(long long i=0;i< dib.width*dib.height;i++)
-{
-    	output[i].r = 255;
-    	output[i].g = 0;
-    	output[i].b = 255;
-
+	//http://en.wikipedia.org/wiki/Selection_algorithm
+	unsigned char minIndex, minValue;
+	for(int i = 0; i < FILTER_WINDOW_SIZE / 2; i++)
+	{
+		minIndex = i;
+		minValue = window[i];
+		for(int j = i + 1; j < FILTER_WINDOW_SIZE; j++)
+		{
+			if(window[j] < minValue)
+			{
+				minIndex = j;
+				minValue = window[j];
+			}
+		}
+		window[minIndex] = window[i]; 
+		window[i] = minValue;
+	}
+	return window[FILTER_WINDOW_SIZE / 2];
 }
 
-
-
-    medianFilterGpu(image, output, dib.width, dib.height);
-
-
-
-
-
-
-    //saving result bmp
-    ofstream bmpResult(OUTPUT_BMP_FILE, ios::out | ios::binary);
-    char *buf = new char[header.offset];
-    char zerosBuf[4] = {0};
-    bmpFile.seekg(0, ios_base::beg);
-    bmpFile.read(buf, header.offset);
-    bmpResult.write(buf, header.offset);
-
-    for(int y = dib.height - 1; y >= 0 ; y--)
-    {
-        for(int x = 0; x < dib.width; x++)
-        {
-            bmpResult.write((char*)&(output[y*dib.width + x]), sizeof(Pixel));
-        }
-        bmpResult.write(zerosBuf, dib.width%4);
-    }
-
-    bmpResult.close();
-    bmpFile.close();
-
-    cout << endl;
-    return 0;
-
+void displayLastError(const string &msg)
+{
+	cout << "Last Error (" << msg << "):\t" << cudaGetErrorString(cudaGetLastError()) << endl;
 }
