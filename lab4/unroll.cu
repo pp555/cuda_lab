7720bb16974c59f9892d70f256c2c9fdf3edbf2f
Piotr Pietruszka <piotrek555@gmail.com>
2012-12-16 20:22:22 +0100

    unroll.cu

--- lab4/unroll.cu
+++ lab4/unroll.cu
@@ -7,9 +7,9 @@
 #include <string>
 
 
-#define BLOCK_SIZE 512
+#define BLOCK_SIZE 128
 #define N 1000
-#define MUL 99999
+#define LOOP 9999
 #define NUMBER_TYPE unsigned int
 
 using namespace std;
@@ -26,9 +26,33 @@ __global__ void kernel(numType *array)
     if(x < N)
     {
         numType num = array[x];
+#pragma unroll 1
+        for(long long int i = 1;i < LOOP;i+=1+2*(i&0x1))
+            array[x] = num*2+3*i/1000.0*i/1000.0;
+    }
+}
+
+__global__ void kernel_unroll(NUMBER_TYPE *array)
+{
+	int x = blockDim.x * blockIdx.x + threadIdx.x;
+    if(x < N)
+    {
+        NUMBER_TYPE num = array[x];
 #pragma unroll 100
-        for(int i = 0;i < MUL-1;i+=3)
-            array[x] += (i%2==0)?num:0;
+        for(long long int i = 1;i < LOOP;i+=1+2*(i&0x1))
+            array[x] = num*2+3*i/1000.0*i/1000.0;
+    }
+}
+
+__global__ void kernel_unroll2(NUMBER_TYPE *array)
+{
+	int x = blockDim.x * blockIdx.x + threadIdx.x;
+    if(x < N)
+    {
+        NUMBER_TYPE num = array[x];
+#pragma unroll 10
+        for(long long int i = 1;i < LOOP;i+=1+2*(i&0x1))
+            array[x] = num*2+3*i/1000.0*i/1000.0;
     }
 }
 
@@ -57,6 +81,7 @@ int main(int argc, char *argv[])
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 
+	//bez unroll
 	cudaEventRecord(start, 0);
     for(int t=0;t<1000;t++)
         kernel<NUMBER_TYPE><<<blocks, BLOCK_SIZE>>>(deviceData);
@@ -67,7 +92,37 @@ int main(int argc, char *argv[])
 
     float kernelTime = 0;
 	cudaEventElapsedTime(&kernelTime, start, stop);
-    cout << "kernel execution time:\t" << kernelTime/1000.0 << endl;
+    cout << "kernel execution time:\t" << kernelTime << endl;
+	
+	//unroll 100
+	cudaEventRecord(start, 0);
+    for(int t=0;t<1000;t++)
+        kernel_unroll<<<blocks, BLOCK_SIZE>>>(deviceData);
+	cudaThreadSynchronize();
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	displayLastError("kernel");
+
+    kernelTime = 0;
+	cudaEventElapsedTime(&kernelTime, start, stop);
+    cout << "kernel execution time:\t" << kernelTime << endl;
+	
+	//unroll 10
+	cudaEventRecord(start, 0);
+    for(int t=0;t<1000;t++)
+        kernel_unroll2<<<blocks, BLOCK_SIZE>>>(deviceData);
+	cudaThreadSynchronize();
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	displayLastError("kernel");
+
+    kernelTime = 0;
+	cudaEventElapsedTime(&kernelTime, start, stop);
+    cout << "kernel execution time:\t" << kernelTime << endl;
+
+
+
+
 
 	cudaMemcpy(data, deviceData, size, cudaMemcpyDeviceToHost);
 	displayLastError("memory copying");
