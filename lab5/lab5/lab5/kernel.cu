fb5445ce113732711e7c6a1d52ea201aa9d05f79
pp555 <piotrek555@gmail.com>
2013-01-20 14:29:16 +0100

    Update lab5/lab5/lab5/kernel.cu

--- lab5/lab5/lab5/kernel.cu
+++ lab5/lab5/lab5/kernel.cu
@@ -1,13 +1,5 @@
 #include "defines.h"
 
-/*
-extern "C" __global__ void IncVect(float *Tin, float *Tout) {
-	int x = blockDim.x * blockIdx.x + threadIdx.x;
-	if(x < N)
-		Tout[x] = Tin[x] + 1;
-}
-*/
-
 extern "C" __global__ void Polynomial(float *result, float *a, float x, int n)
 {
 	int i = blockDim.x * blockIdx.x + threadIdx.x;
