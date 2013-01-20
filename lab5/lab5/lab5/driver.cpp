62c469069e95ab190ea376d329daf94e258d887f
Piotr Pietruszka <piotrek555@gmail.com>
2013-01-20 14:27:28 +0100

    suma po stronie kernela, dane testowe - losowe

--- lab5/lab5/lab5/driver.cpp
+++ lab5/lab5/lab5/driver.cpp
@@ -3,124 +3,112 @@
 #include	<cuda.h>
 
 #include <iostream>
+#include <ctime>
 
 #include 	"defines.h"
 
 #define ALIGN_UP(offset, alignment)  (offset) = ((offset) + (alignment) - 1) & ~((alignment) - 1)
 
+//funkcja liczaca wartosc wielomianu na CPU
 double PolynomialCPU(float *a, float x, int n)
 {
-	double r = 0.0f;
-
-	float *result = new float[n+1];
-
+	float result = 0.0f;
 	for(int i=0;i<n+1;i++)
 	{
-		result[i] = a[i] * pow(x, i);
+		result= (result * x) + a[n - i];
 	}
-
-	for(int i=0;i<n+1;i++)
-		r += double(result[i]);
-
-	delete [] result;
-
-	return r;
+	return result;
 }
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int n = 3;
-	float x = 1;
-	//float a[] = {2, 3, 4};
-	float *a = new float[n+1];
-	float *result = new float[n+1];
-
-	for(int i=0;i<n+1;i++)
-		a[i] = i*0.1;
-
-
-	int blocks = (n+1) / BLK_SZ;
-    if((n+1) % BLK_SZ)
-		blocks++;
-
-	CUdevice	hDevice;
-	CUcontext	hContext;
-	CUmodule	hModule;
-	CUfunction	hFunction;
-
-	CALL( cuInit(0) );
-	CALL( cuDeviceGet(&hDevice, 0) ); 	
-	CALL( cuCtxCreate(&hContext, 0, hDevice) );
-	CALL( cuModuleLoad(&hModule, "kernel.cubin") );
-	CALL( cuModuleGetFunction(&hFunction, hModule, "Polynomial") );
-
-
-	//dane wejsciowe - kopiowanie
-	CUdeviceptr DevA, DevResult;
-
-	CALL( cuMemAlloc(&DevA, (n+1)*sizeof(float) ) );
-	CALL( cuMemAlloc(&DevResult, (n+1)*sizeof(float) ) );
-
-	CALL( cuMemcpyHtoD(DevA, a, (n+1)*sizeof(float)  ) );
-
-
-	CALL( cuFuncSetBlockShape(hFunction, BLK_SZ, 1, 1) );
-
-
-	//przekazanie parametrów do kernela
-	int 	offset = 0;
-	void   *ptr;
-
-	ptr = (void*)(size_t)DevResult;
-	ALIGN_UP(offset, __alignof(ptr));
-	CALL( cuParamSetv(hFunction, offset, &ptr, sizeof(ptr)) );
-	offset += sizeof(ptr);
-
-	ptr = (void*)(size_t)DevA;
-	ALIGN_UP(offset, __alignof(ptr));
-	CALL( cuParamSetv(hFunction, offset, &ptr, sizeof(ptr)) );
-	offset += sizeof(ptr);
-
-	ALIGN_UP(offset, __alignof(float));
-	CALL( cuParamSetf(hFunction, offset, x) );
-	offset += sizeof(float);
-
-	ALIGN_UP(offset, __alignof(int));
-	CALL( cuParamSeti(hFunction, offset, n) );
-	offset += sizeof(int);
-
-
-	CALL( cuParamSetSize(hFunction, offset) );
-
-	CALL( cuLaunchGrid(hFunction, blocks, 1) );
-
-
-	//kopiowanie danych na hosta
-	CALL( cuMemcpyDtoH((void *) result, DevResult, (n+1)*sizeof(float) ) );
-
-
-	CALL( cuMemFree(DevA) );
-	CALL( cuMemFree(DevResult) );
-
-
-	double s = 0.0;
-	for(int i=0;i<n+1;i++)
+	srand(time(NULL));
+	for(int k=0;k<4;k++)
 	{
-		//std::cout << x << '\t' << a[i] << '\t' <<  result[i] << std::endl;
-		s += double(result[i]);
+		int n = 30*(k+1);
+		float x = ((float) rand()) / (float) RAND_MAX;
+		float *a = new float[n+1];
+		float resultGPU;
+
+		for(int i = 0; i < n + 1; i++)
+			a[i] = i * 0.5*((float) rand()) / (float) RAND_MAX;
+
+
+		int blocks = (n + 1) / BLK_SZ;
+		if((n + 1) % BLK_SZ)
+			blocks++;
+
+		CUdevice	hDevice;
+		CUcontext	hContext;
+		CUmodule	hModule;
+		CUfunction	hFunction;
+
+		CALL( cuInit(0) );
+		CALL( cuDeviceGet(&hDevice, 0) ); 	
+		CALL( cuCtxCreate(&hContext, 0, hDevice) );
+		CALL( cuModuleLoad(&hModule, "kernel.cubin") );
+		CALL( cuModuleGetFunction(&hFunction, hModule, "Polynomial") );
+
+
+		//dane wejsciowe - kopiowanie
+		CUdeviceptr DevA, DevResult;
+
+		CALL( cuMemAlloc(&DevA, (n+1)*sizeof(float) ) );
+		CALL( cuMemAlloc(&DevResult, sizeof(float) ) );
+
+		CALL( cuMemcpyHtoD(DevA, a, (n+1)*sizeof(float)  ) );
+
+
+		CALL( cuFuncSetBlockShape(hFunction, BLK_SZ, 1, 1) );
+
+
+		//przekazanie parametrow do kernela
+		int 	offset = 0;
+		void   *ptr;
+
+		ptr = (void*)(size_t)DevResult;
+		ALIGN_UP(offset, __alignof(ptr));
+		CALL( cuParamSetv(hFunction, offset, &ptr, sizeof(ptr)) );
+		offset += sizeof(ptr);
+
+		ptr = (void*)(size_t)DevA;
+		ALIGN_UP(offset, __alignof(ptr));
+		CALL( cuParamSetv(hFunction, offset, &ptr, sizeof(ptr)) );
+		offset += sizeof(ptr);
+
+		ALIGN_UP(offset, __alignof(float));
+		CALL( cuParamSetf(hFunction, offset, x) );
+		offset += sizeof(float);
+
+		ALIGN_UP(offset, __alignof(int));
+		CALL( cuParamSeti(hFunction, offset, n) );
+		offset += sizeof(int);
+
+
+		CALL( cuParamSetSize(hFunction, offset) );
+
+		CALL( cuLaunchGrid(hFunction, blocks, 1) );
+
+
+		//kopiowanie wyniku na hosta
+		CALL( cuMemcpyDtoH((void *) &resultGPU, DevResult, sizeof(float) ) );
+
+
+		//zwalnianie pamieci na urzadzeniu
+		CALL( cuMemFree(DevA) );
+		CALL( cuMemFree(DevResult) );
+
+
+		//obliczenia na CPU
+		float resultCPU = PolynomialCPU(a, x, n);
+
+
+		std::cout << "GPU:\t" << resultGPU << std::endl;
+		std::cout << "CPU:\t" << resultCPU << std::endl;
+		std::cout << "roznica:\t" << fabs(resultGPU - resultCPU) << std::endl;
+		
+		delete [] a;
 	}
-	std::cout << "GPU:\t" << s << std::endl;
-
-
-
-	std::cout << "CPU:\t" << PolynomialCPU(a, x, n) << std::endl;
-
-
-	puts("done");
-
-	delete [] a;
-	delete [] result;
 
 	return 0;
 }
--- lab5/lab5/lab5/kernel.cu
+++ lab5/lab5/lab5/kernel.cu
@@ -11,10 +11,22 @@ extern "C" __global__ void IncVect(float *Tin, float *Tout) {
 extern "C" __global__ void Polynomial(float *result, float *a, float x, int n)
 {
 	int i = blockDim.x * blockIdx.x + threadIdx.x;
+	
+	__shared__ float resultPart[BLK_SZ+1];
 
 	if(i < n + 1)
 	{
-		result[i] = a[i] * pow(x, i);
+		resultPart[i] = a[i] * pow(x, i); //obliczenie czesci wielomianu
 	}
+	__syncthreads();
 
+	//sumowanie wszystkich czesci (w jednym watku)
+	if(i == 0)
+	{
+		float w = 0.0f;
+		for(int j = 0; j < n + 1; j++)
+			w = w + resultPart[j];
+		*result = w;
+	}
+	__syncthreads();
 }
